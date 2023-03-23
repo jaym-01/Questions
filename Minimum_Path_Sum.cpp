01dc16b7b39ab994d7e9703c186e4188e1271e29
jellyware-1 <125736418+jellyware-1@users.noreply.github.com>
2023-03-23 13:00:36 +0000

    same Q

--- Minimum_Path_Sum.cpp
+++ Minimum_Path_Sum.cpp
@@ -1,11 +1,48 @@
+#include <vector>
+#include <iostream>
+
+using namespace std;
+
 class Solution {
 public:
     int minPathSum(vector<vector<int>>& grid) {
-        
+        return traverse(0, 0, grid);
     }
 
     //recursive check every path
-    void traverse(int x, int y, vector<vector<int>>& grid){
-        if(x > grid[y].size() || )
+    int traverse(int x, int y, vector<vector<int>>& grid){
+        if(x < grid[0].size() && x > -1 && y < grid.size() && y > -1){
+
+            int right = traverse(x+1, y, grid);
+            int down = traverse(x, y + 1, grid);
+
+            if(right == 0 && down == 0){
+                return grid[y][x];
+            }
+            else if(right == 0){
+                return grid[y][x] + down;
+            }
+            else if(down == 0){
+                return grid[y][x] + right;
+            }
+            else if(right > down){
+                return grid[y][x] + down;
+            }
+            else{
+                return grid[y][x] + right;
+            }
+        }
+
+        else{
+            return 0;
+        }
     }
 };
+
+int main(){
+    Solution obj;
+
+    vector<vector<int>> grid = {{1,3,1},{1,5,1},{4,2,1}}; 
+
+    cout << obj.minPathSum(grid) << endl;
+}
