42e6333cbad0915d7b5a814155599368ba78b63b
jellyware-1 <125736418+jellyware-1@users.noreply.github.com>
2023-05-17 19:04:15 +0100

    saving

--- first_last_array.cpp
+++ first_last_array.cpp
@@ -11,11 +11,9 @@ public:
             return {-1, -1};
         }
 
-        int len2 = nums.size() - 1 - inx;
-
         int low = inx, high = inx;
 
-        for(int i = 1; i <= max(inx, len2); i++){
+        for(int i = 1; i < nums.size(); i++){
             if(low > -1 && high < nums.size()){
                 if (nums[high + 1] != target && nums[low - 1] != target)
                 {
