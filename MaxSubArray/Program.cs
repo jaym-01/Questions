ac4d2b38970cdea9d3c9e42e14f3c27aca77d0a7
jellyware-1 <125736418+jellyware-1@users.noreply.github.com>
2023-06-14 10:05:27 +0100

    first attempt with time limit exceeded

--- MaxSubArray/Program.cs
+++ MaxSubArray/Program.cs
@@ -1,7 +1,26 @@
 ﻿namespace MaxSubArray{
     class Program{
         public static void Main(string[] args){
-            Console.WriteLine("test");
+            Console.WriteLine(MaxSubArray(new int[]{1}));
+        }
+
+        public static int MaxSubArray(int[] nums) {
+            int max = nums[0], tmp;
+
+            for(int i = 0; i < nums.Length; i++){
+
+                tmp = 0;
+
+                for(int j = i; j < nums.Length; j++){
+                    tmp += nums[j];
+                    if(tmp > max){
+                        max = tmp;
+                    }
+                }
+
+            }
+
+            return max;
         }
     }
 }
