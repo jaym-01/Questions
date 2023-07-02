f15a2aec3d5e07c7e5b6a7c441e03d019f9973d0
jellyware-1 <jay_mistry@live.co.uk>
2023-07-02 18:17:04 +0100

    completed then tried to optimise

---
+++ maximal_square.cpp
@@ -0,0 +1,83 @@
+#include <iostream>
+#include <vector>
+
+using namespace std;
+
+class Solution {
+public:
+    int maximalSquare(vector<vector<char>>& matrix) {
+        int max = 0;
+
+        for(int i = 0; i < matrix.size(); i++){
+            for(int j = 0; j < matrix[i].size(); j++){
+                if(matrix[i][j] != '0' && matrix[i][j] != 'x'){
+                    if(check_square(max + 1, i, j, matrix)){
+                        max++;
+                        while(check_expansion(max + 1, i, j, matrix)){
+                            max++;
+                        }
+                    }
+                    // while(check_square(max + 1, i, j, matrix)){
+                    //     max++;
+                    // }
+                }
+            }
+        }
+
+        return max * max;
+    }
+
+private:
+    bool check_square(int size, int i, int j, vector<vector<char>>& matrix){
+        if(size + i > matrix.size() || size + j > matrix[i].size()){
+            return false;
+        }
+
+        for(int y = i; y < i + size; y++){
+            for(int x = j; x < j + size; x++){
+                if(matrix[y][x] == '0'){
+                    return false;
+                }
+                else{
+                    continue;
+                }
+            }
+        }
+
+        return true;
+    }
+
+    bool check_expansion(int size, int i, int j, vector<vector<char>>& matrix){
+        // int x = j, y = i;
+
+        if(size + i > matrix.size() || size + j > matrix[i].size()){
+            return false;
+        }
+        
+        signed int x = j + size - 1, y;
+
+        for(y = i; y < i + size; y++){
+            if(matrix[y][x] == '0'){
+                return false;
+            }
+        }
+
+        --y;
+
+        for(; x > j - size + 1; --x){
+            if(matrix[y][x] == '0'){
+                return false;
+            }
+        }
+
+        return true;
+    }
+};
+
+int main(){
+    Solution s1;
+
+    vector<vector<char>> test1 {{'1', '1'}, {'1', '1'}};
+
+    cout << s1.maximalSquare(test1) << endl;
+}
