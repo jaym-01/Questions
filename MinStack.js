730db4bd3a55ba1ce32b62d4d2144a0d83cf00fb
jellyware-1 <125736418+jellyware-1@users.noreply.github.com>
2023-07-07 11:24:43 +0100

    partially complete

---
+++ MinStack.js
@@ -0,0 +1,63 @@
+var MinStack = function() {
+    this.content = [];
+    this.top_val = -1;
+    // this.min = null;
+};
+
+/** 
+ * @param {number} val
+ * @return {void}
+ */
+MinStack.prototype.push = function(val) {
+    if(this.content.length == this.top_val + 1){
+        this.content.push(val);
+    }
+    else{
+        this.content[this.top_val + 1] = val;
+    }
+    this.top_val++;
+};
+
+/**
+ * @return {void}
+ */
+MinStack.prototype.pop = function() {
+    if(this.top_val > -1){
+        --this.top_val;
+    }    
+};
+
+/**
+ * @return {number}
+ */
+MinStack.prototype.top = function() {
+    if(this.top_val > -1){
+        return this.content[this.top_val];
+    }
+};
+
+/**
+ * @return {number}
+ */
+MinStack.prototype.getMin = function() {
+    return Math.min(this.content);
+};
+
+/** 
+ * Your MinStack object will be instantiated and called as such:
+ * var obj = new MinStack()
+ * obj.push(val)
+ * obj.pop()
+ * var param_3 = obj.top()
+ * var param_4 = obj.getMin()
+ */
+
+var obj = new MinStack()
+console.log(obj.push(3));
+console.log(obj.push(4));
+console.log(obj.push(7));
+console.log(obj.top());
+console.log(obj.pop());
+console.log(obj.top());
+console.log(obj.push(8));
+console.log(obj.MinStack);
