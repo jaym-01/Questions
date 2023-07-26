e5826f80577b168f76fde23d34a6e67809947543
jellyware-1 <jay_mistry@live.co.uk>
2023-07-26 14:12:05 +0100

    try 1

---
+++ 2022_july/regular_expression_matching.cpp
@@ -0,0 +1,61 @@
+#include <iostream>
+#include <string>
+
+using namespace std;
+
+
+class Solution {
+public:
+    bool isMatch(string s, string p) {
+        int s_idx = 0;
+
+        for(int i = 0; i < p.size(); i++){
+
+            if(i != p.size() - 1 && p[i+1] == '*'){
+
+                if(p.size() - i == 2){
+                    return true;
+                }
+
+                bool first = true;
+
+                while(s_idx < s.size() && (p[i] != '.' && (s[s_idx] == p[i] || first) || p[i] == '.')){
+
+                    first = false;
+
+                    if(!isMatch(s.substr(s_idx, s.size() - s_idx), p.substr(i + 2, p.size() - i + 2))){
+                        s_idx++;
+                    }
+                    else{
+                        return true;
+                    }
+                }
+
+                i++;
+            }
+            else if(p[i] != '.'){
+                if(s[s_idx] != p[i]){
+                    return false;
+                }
+            }
+
+            s_idx++;
+        }
+
+        if(s_idx != s.size()){
+            return false;
+        }
+
+        return true;
+    }
+};
+
+int main(){
+    Solution sol;
+    
+    string s1 = "aaaaaaaaaa";
+    string p1 = "a*a";
+
+    cout << sol.isMatch(s1, p1) << endl;
+
+}
