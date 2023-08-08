a1291bffd3ce8a1e1e4232e661c59b6b8175f198
jellyware-1 <125736418+jellyware-1@users.noreply.github.com>
2023-08-08 21:26:39 +0100

    done

--- 2022_august/rotated_array_search.cpp
+++ 2022_august/rotated_array_search.cpp
@@ -38,54 +38,81 @@ public:
             }
         }
 
-        bool iterate = true;
+        int out;
 
-        int sub_len = 0, mid;
+        out = bs(nums, target, low, nums.size() - 1);
 
-        while(iterate){
-            if(high == low){
-                iterate = false;
-            }
-
-            if(high < low){
-                sub_len = nums.size() - low + high + 1; // add 1 since high and low are indexes
-
-                mid = (sub_len / 2 + low) % nums.size();
-            }
-            else if(high > low){
-                // sub_len = high - low;
-                mid = (high + low)/2;
-            }
-            else{
-                mid = high;
-            }
-
-            if(nums[mid] == target){
-                return mid;
-            }
-            else if(nums[mid] > target){
-                high = mid - 1;
-                if(high <= low && high < 0){
-                    high = nums.size() + high; 
-                }
-                else if(high > low){
-                    break;
-                }
-
-            }
-            else{
-                low = (mid + 1) % nums.size();
-
-            }
+        if(out == -1){
+            return bs(nums, target, 0, high);
+        }
+        else{
+            return out;
         }
 
-        return -1;
+        // bool iterate = true;
+
+        // int sub_len = 0, mid;
+
+        // while(iterate){
+        //     if(high == low){
+        //         iterate = false;
+        //     }
+
+        //     if(high < low){
+        //         sub_len = nums.size() - low + high + 1; // add 1 since high and low are indexes
+
+        //         mid = (sub_len / 2 + low) % nums.size();
+        //     }
+        //     else if(high > low){
+        //         // sub_len = high - low;
+        //         mid = (high + low)/2;
+        //     }
+        //     else{
+        //         mid = high;
+        //     }
+
+        //     if(nums[mid] == target){
+        //         return mid;
+        //     }
+        //     else if(nums[mid] > target){
+        //         high = mid - 1;
+        //         if(high < 0){
+        //             high = nums.size() + high; 
+        //         }
+
+        //     }
+        //     else{
+        //         low = (mid + 1) % nums.size();
+
+        //     }
+        // }
+
+        // return -1;
+    }
+
+private:
+
+    int bs(vector<int>& nums, int target, int low, int high){
+        if(high < low){
+            return -1;
+        }
+        else{
+            int mid = (high + low) / 2;
+
+            if(nums[mid] == target) return mid;
+            else if(nums[mid] > target){
+                return bs(nums, target, low, mid - 1);
+            }
+            else{
+                return bs(nums, target, mid + 1, high);
+            }
+        }
     }
 };
 
 int main(){
     Solution s1;
-    vector<int> t1 {1,2,3,6,7,9};
+    vector<int> t1 {4,5,6,7,8,1,2,3};
 
-    cout << s1.search(t1, 0) << endl; 
+    cout << s1.search(t1, 8) << endl; 
 }
