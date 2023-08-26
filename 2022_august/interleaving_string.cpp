19c6612d3e88576a345a3d0757de9cd5c8df16be
jellyware-1 <125736418+jellyware-1@users.noreply.github.com>
2023-08-26 21:29:59 +0100

    done with the answer

--- 2022_august/interleaving_string.cpp
+++ 2022_august/interleaving_string.cpp
@@ -7,63 +7,41 @@ using namespace std;
 class Solution {
 public:
     bool isInterleave(string s1, string s2, string s3) {
-        vector<vector<int>> mem = vector<vector<int>>(s1.size() + 1, vector<int>(s2.size() + 1, -1));
-        return cut(s1, s2, s3, 0, 0, mem);
-    }
-
-private:
-    bool cut(string s1, string s2, string s3, int n, int m, vector<vector<int>> mem){
-        if(s1 == "" && s2 == "" && s3 == "" && abs(n - m) <= 1){
-            return true;
-        }
-        else if(s1.size() + s2.size() != s3.size()){
+        if(s1.size() + s2.size() != s3.size()){
             return false;
         }
-        else if(mem[s1.size()][s2.size()] != -1){
-            return mem[s1.size()][s2.size()];
+
+        vector<vector<bool>> mem = vector<vector<bool>>(s1.size() + 1, vector<bool>(s2.size() + 1, false));
+        
+        //the empty s1 and empty s2 are both the same as the empty s3
+        //also need it to work for the algorithm
+        mem[0][0] = true;
+
+        // How much of the start of s1 is the same as s3 - once its different - the rest of s1 combinations become false
+        for(int i = 1; i < s1.size(); i++){
+            mem[i][0] = mem[i-1][0] && s1[i-1] == s3[i-1];
         }
 
-        int i = 0;
-        bool result;
+        // same for s2
+        for(int j = 1; j < s2.size(); j++){
+            mem[0][j] = mem[0][j-1] && s2[j-1] == s3[j-1];
+        }
 
-        if(s1.size() > 0 && s1[0] == s3[0]){
-            while(i < s1.size() && s1[i] == s3[i]) i++;
+        for(int i = 1; i <= s1.size(); i++){
+            for(int j = 1; j <= s2.size(); j++){
+                // checking for splits that could occur
+                // checking the current index of s3 -> which is currently made up of  s1 and s2 -> but can be mixed up in any way
 
-            result = false;
-
-            while(i > 0){
-                if(s2.size() == 0 || (s2.size() > 0 && i < s3.size() && s2[0] == s3[i])){
-                    result = cut(s1.substr(i, s1.size() - i), s2, s3.substr(i, s3.size() - i), n + 1, m, mem);
-                    if(result) return true;
-                }
-                mem[s1.size() - i][s2.size()] = result;
-                --i;
+                mem[i][j] = (mem[i-1][j] && s1[i-1] == s3[i+j-1]) || (mem[i][j-1] && s2[j-1] == s3[i+j-1]);
             }
         }
 
-        i = 0;
-        if(s2.size() > 0 && s2[0] == s3[0]){
-            while(i < s2.size() && s2[i] == s3[i]) i++;
-
-            result = false;
-
-            while(i > 0){
-                if(s1.size() == 0 || (s1.size() > 0 && i < s3.size() && s1[0] == s3[i])){
-                    result = cut(s1, s2.substr(i, s2.size() - i), s3.substr(i, s3.size() - i), n, m + 1, mem);
-                    if(result) return true;
-                }
-                mem[s1.size()][s2.size() - i] = result;
-                --i;
-            }
-        }
-
-        mem[s1.size()][s2.size()] = false;
-        return false;
+        return mem[s1.size()][s2.size()];
     }
 };
 
 int main(){
     Solution s;
 
-    cout << s.isInterleave("aabcc", "dbbca", "aadbbcbcac") << endl;
+    cout << s.isInterleave("bbbbbabbbbabaababaaaabbababbaaabbabbaaabaaaaababbbababbbbbabbbbababbabaabababbbaabababababbbaaababaa", "babaaaabbababbbabbbbaabaabbaabbbbaabaaabaababaaaabaaabbaaabaaaabaabaabbbbbbbbbbbabaaabbababbabbabaab", "babbbabbbaaabbababbbbababaabbabaabaaabbbbabbbaaabbbaaaaabbbbaabbaaabababbaaaaaabababbababaababbababbbababbbbaaaabaabbabbaaaaabbabbaaaabbbaabaaabaababaababbaaabbbbbabbbbaabbabaabbbbabaaabbababbabbabbab") << endl;
 }
