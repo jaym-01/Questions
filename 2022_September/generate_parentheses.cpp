e1a35302b0a4cd2b2f386bdd0d518086ad37daa0
jellyware-1 <125736418+jellyware-1@users.noreply.github.com>
2023-09-09 13:46:47 +0100

    completed

--- 2022_September/generate_parentheses.cpp
+++ 2022_September/generate_parentheses.cpp
@@ -26,6 +26,31 @@ public:
         if(brackets.size() == 0){
             if(!mem.contains(group) && valid(group, mem)) out.push_back(group);
         }
+        else if(mem.contains(group)){
+            return;
+        }
+        else{
+            int end;
+            if(first) end = brackets.size() / 2;
+            else end = brackets.size(); 
+
+            for(int i = 0; i < end; i++){
+                // partition vector
+                vector<char> left = vector<char>(brackets.begin(), brackets.begin() + i), right = vector<char>(brackets.begin() + i + 1, brackets.end());
+                
+                left.insert(left.end(), right.begin(), right.end());
+
+                all_combos(left, mem, group + brackets[i], out, false);
+            }
+
+            mem[group] = false;
+        }
+    }
+
+    void all_combos_old(vector<char> brackets, map<string, bool>& mem, string group, vector<string>& out, bool first){
+        if(brackets.size() == 0){
+            if(!mem.contains(group) && valid(group, mem)) out.push_back(group);
+        }
         else{
             int end;
             if(first) end = brackets.size() / 2;
@@ -59,9 +84,9 @@ public:
 int main(){
     Solution s;
 
-    vector<string> out = s.generateParenthesis(5);
+    vector<string> out = s.generateParenthesis(8);
 
     for(int i = 0; i < out.size(); i++){
-        cout << out[i] << endl;
+        cout << i << " | " << out[i] << endl;
     }
 }
